322fe207e628ce0fbc020648960152462ef0e06c
David McCallie <dmccallie@gmail.com>
2024-09-27 19:02:07 -0500

    preload server at startup, move server list and telescope to memory cache, refactor index page

--- app.py
+++ app.py
@@ -7,55 +7,104 @@ from telescope import CacheTelescope, AlpycaTelescope, get_servers
 from alpaca.telescope import DriveRates, TelescopeAxes
 
 app = Flask(__name__)
+shared_servers_cache = {} # global dictionary to hold server and telescope data
+
 
 # initial page puts up note about searching for servers, starts HTMX GET
 #  for server_search
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # assuming we have pre-fetched the server list, show the pick list to user
+    print("Index page called with shared_servers_cache: ", shared_servers_cache)
+    if 'shared_server_list' in shared_servers_cache:
+        servers = shared_servers_cache['shared_server_list']
+        print("Servers found: ", servers)
+        if len(servers) > 0:
+            servers_found = True
+            # make first choice in combo box a "Select a server" choice - better way to do this?????
+            servers_for_combo = [{'index': 0, 'name': 'Select a server'}]
+            for i, server in enumerate(servers):
+                new_server = {}
+                servers_for_combo.append(new_server)
+                new_server['index'] = i+1
+                new_server['name'] = f"{servers[i][0]} - {servers[i][1]}" 
+        else:
+            servers_found = False
+    else:
+        servers_found = False
+        servers_for_combo = []
 
-# sever_search is called by HTMX GET to get the list of servers
-#  populates combo and swaps into the index page
-@app.route('/server_search', methods=['GET'])
-def server_search():
-    print("Server search started")
-    servers = get_servers() # use alpaca.discovery to get available servers
-    # servers is [ (ip_address, server name ) ]
-    print("Servers found: ", servers)
-    servers_for_combo = [{'index': 0, 'name': 'Select a server'}]
-    for i, server in enumerate(servers):
-        new_server = {}
-        servers_for_combo.append(new_server)
-        new_server['index'] = i+1
-        new_server['name'] = f"{servers[i][0]} - {servers[i][1]}" 
-
-    servers_found = len(servers) > 0
-
-    return render_template('select_server.html',  servers_found=servers_found,
+    return render_template('index.html', servers_found=servers_found,
                            servers_for_combo=servers_for_combo)
-  
-    # return render_template('index.html', 
-    #             coords={'altitude': 0, 'azimuth': 0, 'ra': 0, 'dec': 0},
-    #             servers=servers_for_combo, servers_found=True)
 
+# # sever_search is called by HTMX GET to get the list of servers
+# # populates combo and swaps into the index page
+# # don't need this with pre-fetching server list
+# @app.route('/server_search', methods=['GET'])
+# def server_search():
+    
+#     if 'servers' in shared_servers_cache:
+#         servers = shared_servers_cache['shared_server_list']
+#         print("Cached Servers found: ", servers)
+#     else:
+#         print("Server search started")
+#         shared_servers_cache['shared_servers'] = get_servers()
+#         servers = shared_servers_cache['shared_server_list']
+#         print("Server search returns: ", servers)
+
+#     servers_found = len(servers) > 0
+
+#     servers_for_combo = [{'index': 0, 'name': 'Select a server'}]
+#     for i, server in enumerate(servers):
+#         new_server = {}
+#         servers_for_combo.append(new_server)
+#         new_server['index'] = i+1
+#         new_server['name'] = f"{servers[i][0]} - {servers[i][1]}" 
+
+
+
+#     return render_template('select_server.html',  servers_found=servers_found,
+#                            servers_for_combo=servers_for_combo)
+  
 # server_select is called when a server is selected from the combo box using POST
-#  then redirects to the paddle control page
+# then redirects to the paddle control page
 @app.route('/server_select', methods=['POST'])
 def server_select():
     print("Server selected with form = ", request.form)
     selected_option = request.form.get('servers-combo-box')
-    # Handle the selected option here
     print(f"Selected server option: {selected_option}")
-    # return '', 204  # Empty response, HTMX does not require content.
+    if selected_option == '0':
+        return redirect(url_for('index'))
+    else:
+        selected_server = shared_servers_cache['shared_server_list'][int(selected_option)-1]
+        print(f"Selected server: {selected_server}")
+        # create telescope object and cache it
+        try:
+            telescope = AlpycaTelescope(app, selected_server[0]) # selected_server[0] is the IP address
+            shared_servers_cache['telescope'] = telescope
+        except Exception as e:
+            print(f"Error connecting to telescope at {selected_server} error= {e}")
+            return render_template('error.html', message="Error connecting to telescope")
+        
     return redirect(url_for('paddle'))
 
 # page to display paddle controls and telescope status
 @app.route('/paddle')
 def paddle():
+
+    # should be connected to a telescope by now
+    telescope: AlpycaTelescope = shared_servers_cache['telescope']
+
     # get the tracking rates supported by the telescope
     tracking_rates:list[DriveRates] = telescope.get_tracking_rates()
     for tr in tracking_rates:
         print(f"Tracking rate: {tr.name} = {tr.value}")
+
+    # get whether tracking is turned on
+    tracking = telescope.is_tracking()
+
+    # get current tracking rate
+    current_tracking_rate = telescope.get_tracking_rate()
 
     # get the slewing rates supported by the telescope
     # just check RA, assume same for DEC
@@ -67,11 +116,16 @@ def paddle():
 
     return render_template('paddle.html', coords={'altitude': 0, 'azimuth': 0, 
             'ra': 0, 'dec': 0, 'slewing': False, 'tracking': False, 
-            'tracking_rate': 0}, tracking_rates=tracking_rates, slewing_rates=slewing_rates)
+            'tracking_rate': 0}, tracking_rates=tracking_rates, 
+                current_tracking_rate=current_tracking_rate,
+                slewing_rates=slewing_rates, tracking=tracking)
 
 # control is called when a control button is pressed
 @app.route('/control', methods=['POST'])
 def control():
+    # get the telescope object from the cache
+    telescope: AlpycaTelescope = shared_servers_cache['telescope']
+
     direction = request.form.get('direction')
     event_type = request.form.get('event_type')
     current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
@@ -98,44 +152,68 @@ def control():
 
 @app.route('/update_tracking', methods=['POST'])
 def update_tracking():
-    rate = request.form.get('tracking-rate')
-    if rate:
-        rate = int(rate)
-    else:
-        rate = 0
-    print(f"Setting tracking rate to {rate}")
-    tracking_on_off = request.form.get('tracking-enable')
-    print("set tracking on/off to: ", tracking_on_off)
-    if tracking_on_off == 'true':
-        telescope.set_tracking(True)
-    else:
-        telescope.set_tracking(False)
-    telescope.set_tracking_rate(DriveRates(rate))
+    # get the telescope object from the cache
+    telescope: AlpycaTelescope = shared_servers_cache['telescope']
+
+    if telescope is not None:
+        rate = request.form.get('tracking-rate')
+        if rate:
+            rate = int(rate)
+        else:
+            rate = 0
+        print(f"Setting tracking rate to {rate}")
+        tracking_on_off = request.form.get('tracking-enable')
+        print("set tracking on/off to: ", tracking_on_off)
+        if tracking_on_off == '1':
+            telescope.set_tracking(True)
+        else:
+            telescope.set_tracking(False)
+        telescope.set_tracking_rate(DriveRates(rate))
     return '', 204
 
 @app.route('/status', methods=['GET'])
 def status():
-    coords = telescope.get_coords()
-    # coords = telescope.cache.get('coords')
-    coords['slewing'] = telescope.is_slewing()
-    coords['tracking'] = telescope.is_tracking()
-    coords['tracking_rate'] = telescope.get_tracking_rate()
+    
+    # get the telescope object from the cache
+    telescope: AlpycaTelescope = shared_servers_cache['telescope']
 
+    if telescope is None:
+        coords = {'altitude': 0, 'azimuth': 0, 'ra': 0, 'dec': 0}
+        coords['slewing'] = False
+        coords['tracking'] = False
+        coords['tracking_rate'] = 0
+    else:
+        coords = telescope.get_coords()
+        coords['slewing'] = telescope.is_slewing()
+        coords['tracking'] = telescope.is_tracking()
+        coords['tracking_rate'] = telescope.get_tracking_rate()
+    
     return render_template('telescope_coords.html', coords=coords)
 
 if __name__ == '__main__':
 
     # create shared telescope object
-    try:
-        # telescope = CacheTelescope(app)
-        telescope = AlpycaTelescope(app)
+    # will be shared across users, since there is only one telescope
+    # try:
+    #     # telescope = CacheTelescope(app)
+    #     telescope = AlpycaTelescope(app)
     
-    except RuntimeError as e:
-        print("Initializing telescope failed: ", e)
+    # except RuntimeError as e:
+    #     print("Initializing telescope failed: ", e)
+    #     sys.exit(1)
+
+    # create a shared global object to hold the server location data (discovery data)
+    # this object will be shared across users as well, since they are all on the same LAN
+    # do that when app starts up, since it is a one-time operation
+    try:
+        servers = get_servers()
+        # if this were a multi-server app, we'd need to use Redis or some other shared cache
+        shared_servers_cache['shared_server_list'] = servers  # cache in memory, will be shared across users
+
+        shared_servers_cache['telescope'] = None # can't create telescope until user has selected a server
+
+    except Exception as e:
+        print(f"Error getting servers: {e}")
         sys.exit(1)
-
-    print("Telescope found - starting app")
-
-    # discover services
 
     app.run(debug=True, port=5001)
